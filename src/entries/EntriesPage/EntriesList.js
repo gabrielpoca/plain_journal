6bffd378644237a7b1b17c254ae02abc603fc1d3
Gabriel Poça <gabrielpoca@gmail.com>
2019-01-20 10:21:24 +0000

    limit the amount of text in entry listing

--- src/entries/EntriesPage/EntriesList.js
+++ src/entries/EntriesPage/EntriesList.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import moment from 'moment';
+import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+
+const Text = styled(Typography)`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
 const Entry = ({ entry }) => {
   const template = document.createElement('div');
-  template.innerHTML = entry.body;
+  template.innerHTML = entry.body.split('</p>')[0] + '</p>';
 
   return (
     <ListItem component={Link} to={`/entries/entry/${entry._id}`}>
-      <ListItemText
-        primary={template.innerText}
-        secondary={moment(entry.date).format('DD/MM/YY')}
-      />
+      <ListItemText secondary={moment(entry.date).format('DD/MM/YY')}>
+        <Text>{template.innerText}</Text>
+      </ListItemText>
     </ListItem>
   );
 };
