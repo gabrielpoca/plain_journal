f0082560a4473751ddef3fdc1676142077eb0479
Gabriel Poça <gabrielpoca@gmail.com>
2019-07-21 18:06:07 +0100

    update logo

--- public/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-firebase.initializeApp({
-  messagingSenderId: '%REACT_APP_SENDER_ID%',
-});
-
-const messaging = firebase.messaging();
