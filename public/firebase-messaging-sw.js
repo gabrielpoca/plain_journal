0c73c0bfddb5d8099ddb5d875d64b5c56014d888
Gabriel Poça <gabrielpoca@gmail.com>
2019-09-13 10:59:20 +0100

    firebase configuration

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,16 @@
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
 
-firebase.initializeApp({
-  messagingSenderId: "223437832654"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyAPj6p8DQLx9er2mSUHgHQb1aZK2HmDxAw",
+  authDomain: "diary-5ef53.firebaseapp.com",
+  databaseURL: "https://diary-5ef53.firebaseio.com",
+  projectId: "diary-5ef53",
+  storageBucket: "diary-5ef53.appspot.com",
+  messagingSenderId: "223437832654",
+  appId: "1:223437832654:web:289e30e4892bcd45c72b9f"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
--- src/notifications.js
+++ src/notifications.js
@@ -3,10 +3,18 @@ import "firebase/messaging";
 
 window.firebase = firebase;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAPj6p8DQLx9er2mSUHgHQb1aZK2HmDxAw",
+  authDomain: "diary-5ef53.firebaseapp.com",
+  databaseURL: "https://diary-5ef53.firebaseio.com",
+  projectId: "diary-5ef53",
+  storageBucket: "diary-5ef53.appspot.com",
+  messagingSenderId: "223437832654",
+  appId: "1:223437832654:web:289e30e4892bcd45c72b9f"
+};
+
 export const initializeFirebase = () => {
-  firebase.initializeApp({
-    messagingSenderId: process.env.REACT_APP_SENDER_ID
-  });
+  firebase.initializeApp(firebaseConfig);
 };
 
 export const askForPermissioToReceiveNotifications = async () => {
